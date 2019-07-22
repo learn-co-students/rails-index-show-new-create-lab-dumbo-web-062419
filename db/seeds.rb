a9a61ca5c63fe0314ee83960753e91cdc7f01411
Dennis Wang <dwang0816@gmail.com>
2019-07-22 11:27:31 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+coupon1 = Coupon.create(coupon_code: "23kKJ3", store:"store1")
+coupon1 = Coupon.create(coupon_code: "34LJHk", store:"store2")
+coupon1 = Coupon.create(coupon_code:"LIJhjb", store:"store3")
+coupon1 = Coupon.create(coupon_code:"3487ih", store:"store4")
+coupon1 = Coupon.create(coupon_code:"23rfaf", store:"store5")
+coupon1 = Coupon.create(coupon_code:"3r23wr", store:"store6")
